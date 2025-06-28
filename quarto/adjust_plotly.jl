aa8e9e04ca151538394da95e1cbc811f662a9c7a
jverzani <jverzani@gmail.com>
2025-06-27 21:02:20 -0400

    script to adjust plotly position in loading of javascript

--- quarto/adjust_plotly.jl
+++ quarto/adjust_plotly.jl
@@ -5,19 +5,40 @@
 # This little script just adds a line *before* the require call
 # which seems to make it all work. The line number 83 might change.
 
-f = "_book/alternatives/plotly_plotting.html"
-lineno = 88
+#alternatives/plotly_plotting.html
+function _add_plotly(f)
+    lineno = 117
 
 str = """
-
 <script src="https://cdn.plot.ly/plotly-2.11.0.min.js"></script>
-
 """
 
-r = readlines(f)
-open(f, "w") do io
-    for (i,l) ∈ enumerate(r)
-        i == lineno && println(io, str)
-        println(io, l)
+    r = readlines(f)
+    open(f, "w") do io
+        for (i,l) ∈ enumerate(r)
+            i == lineno && println(io, str)
+            println(io, l)
+        end
     end
 end
+
+
+
+function (@main)(args...)
+    for (root, dirs, files) in walkdir("_book")
+        for fᵢ ∈ files
+            f = joinpath(root, fᵢ)
+            if endswith(f, ".html")
+                @show :adjust, f
+                _add_plotly(f)
+            end
+        end
+    end
+
+    #f = "_book/integrals/center_of_mass.html"
+    #_add_plotly(f)
+
+    return 1
+end
+
+ ["ODEs", "alternatives", "derivatives", "differentiable_vector_calculus", "integral_vector_calculus", "integrals", "limits", "misc", "precalc", "site_libs"]
